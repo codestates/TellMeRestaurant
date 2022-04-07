5040dc7987b66868393f9bbaf307e633e4d4f0f9
LEE JAE SHIN <eoyld1231@gmail.com>
2022-04-07 17:19:30 +0900

    login add

---
+++ client/src/components/Main.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Main() {
+  return (
+    <div className="">
+      <div className="Main">asdf</div>
+      <div className="Main">asdf</div>
+      <div className="Main">asdf</div>
+    </div>
+  );
+}
+export default Main;
