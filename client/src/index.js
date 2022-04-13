95cc7267beaee65b41649566cbfba7d57366d8aa
LEE JAE SHIN <eoyld1231@gmail.com>
2022-04-13 14:59:10 +0900

    Index.js고침

--- client/src/index.js
+++ client/src/index.js
@@ -2,15 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { Provider } from "react-redux";
-import store from "./store/store";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
+  <React.StrictMode>
     <App />
-  </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
